9552c4f00fb9a26dd2fba82fe9a776339410c790
AkXbots <122038135+AkXbots@users.noreply.github.com>
2023-06-19 11:46:20 +0530

    Update extraction.py
    Signed-off-by: AkXbots <122038135+AkXbots@users.noreply.github.com>

--- OrekiRobot/modules/helper_funcs/extraction.py
+++ OrekiRobot/modules/helper_funcs/extraction.py
@@ -4,7 +4,7 @@ from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
 from OrekiRobot import LOGGER
-from OrekiRobot.modules.users import get_user_id
+
 
 
 def id_from_reply(message):
